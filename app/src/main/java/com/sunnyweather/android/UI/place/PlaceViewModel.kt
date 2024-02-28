750a87b5087473a0cee7b7624ed5f90dcd747b6f
caoyujiao <jojocaoyujiao@didiglobal.com>
2024-02-28 20:18:22 +0800

    [feat]: UI开发

---
+++ app/src/main/java/com/sunnyweather/android/UI/place/PlaceViewModel.kt
@@ -0,0 +1,18 @@
+package com.sunnyweather.android.UI.place
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.Transformations
+import com.sunnyweather.android.logic.Repository
+import com.sunnyweather.android.logic.model.Place
+
+class PlaceViewModel: ViewModel() {
+    private val searchLiveData = MutableLiveData<String>()
+    val placeList = ArrayList<Place>()
+    val placeLiveData = Transformations.switchMap(searchLiveData) { query ->
+        Repository.searchPlaces(query)
+    }
+    fun searchPlaces(query: String) {
+        searchLiveData.value = query
+    }
+}
